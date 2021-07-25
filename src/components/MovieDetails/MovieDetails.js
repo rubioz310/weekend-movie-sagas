037488e615641ecf5080455e89e9bcafe9a07ee9
Jose Rubio <ru310z@gmail.com>
2021-07-24 22:18:39 -0500

    Extra styling touches for movieDetails

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,10 +1,9 @@
-import { Card, CardContent, CardHeader, Divider, Grid, makeStyles, Paper, TextareaAutosize, Typography } from "@material-ui/core";
+import { Card, CardContent, CardHeader, Chip, Divider, Grid, makeStyles, Paper, TextareaAutosize, Typography } from "@material-ui/core";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Popper } from "@material-ui/core";
-import { color } from "@material-ui/system";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -16,6 +15,9 @@ const useStyles = makeStyles(theme => ({
     },
     card:{
         background: '#ffebcd'
+    },
+    poster:{
+        width: '280px'
     }
 }))
 
@@ -31,36 +33,40 @@ function MovieDetails() {
         })
       },[]);
 
-      //Takes care of opening the popper
-      const [anchorEl, setAnchorEl] = useState(null);
-      const handleClick = (event) => {
-        setAnchorEl(anchorEl ? null : event.currentTarget);
-      };
-      const open = Boolean(anchorEl);
-
     return (
         <Paper elevation={4} className={classes.root}>
             <Card className={classes.card}>
                 <CardHeader title={movieDetails.title} className={classes.cardHeader}/>
                 <CardContent>
                     <Grid container item xs={12} >
-                        <Grid item xs={12} sm={4} md={3}><img src={movieDetails.poster} onClick={handleClick} /></Grid>
+                        <Grid item xs={12} sm={4} md={3}>
+                            <img src={movieDetails.poster} className={classes.poster}/>
+                        </Grid>
                         <Grid item xs={12} sm={8} md={9}>
                             <Typography>Description {movieDetails.description}</Typography>
-                            <Divider/>
-                            <Typography>GENRES</Typography>
+                            <Divider />
+                            <Typography><br/>GENRES</Typography>
                             {movieDetails.genres.map(genre =>(
-                                <p>{genre}</p>
+                                <Chip label={genre} />
                             ))}
                         </Grid>
                     </Grid>
                 </CardContent>
             </Card>
-            <Popper open={open} anchorEl={anchorEl} placement='right-end'>
-                <TextareaAutosize value={movieDetails.poster} rows='4' />
-            </Popper>
+            
         </Paper>
     )
 }
 
 export default MovieDetails;
+
+// // //Takes care of opening the popper
+// // const [anchorEl, setAnchorEl] = useState(null);
+// // const handleClick = (event) => {
+// //   setAnchorEl(anchorEl ? null : event.currentTarget);
+// // };
+// // const open = Boolean(anchorEl);
+
+// {/* <Popper open={open} anchorEl={anchorEl} placement='right-end'>
+//                 <TextareaAutosize value={movieDetails.poster} rows='4' />
+//             </Popper> */}
